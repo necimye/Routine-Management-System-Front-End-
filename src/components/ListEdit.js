b3ef0a5f1c26ec9e84bb59a3b53b843e3c9d0118
Sumit <sumitbhagat329@gmail.com>
2020-11-05 11:53:33 +0545

    Start

--- src/components/ListEdit.js
+++ src/components/ListEdit.js
@@ -1,117 +1,109 @@
 import moment from 'moment';
 
 
-const weekdays= ["Select the Day of Week","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekdays = ["Select the Day of Week", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function getToday()
-{
-    const dt= new Date();
+function getToday() {
+    const dt = new Date();
     return dt.getDay() + 1;
 }
 
-function comp(a,b)
-{
-    if( Date.parse(moment(`${a.DAY} ${a.START_TIME}`,"dddd h:mm a")) > Date.parse(moment(`${b.DAY} ${b.START_TIME}`,"dddd h:mm a")) ) return 1;
+function comp(a, b) {
+    if (Date.parse(moment(`${a.DAY} ${a.START_TIME}`, "dddd h:mm a")) > Date.parse(moment(`${b.DAY} ${b.START_TIME}`, "dddd h:mm a"))) return 1;
     else return -1;
 }
 
 const ListEdit = {
 
-    addLecture: async function(uuid,course_code,course_name,link1,link2,link3,starttime,endtime,day,type) {
+    addLecture: async function (uuid, subjectName, teacherName, classCode, classGroup, noOfPeriod, courseCode, link1, startTime, endTime, day, type) {
 
-        var list = await localStorage.getItem(type);
-        if(list===null)
-        {
-                list={};
-                await localStorage.setItem(type,JSON.stringify(list));
+        var list = await localStorage.getItem('class');
+        if (list === null) {
+            list = {};
+            await localStorage.setItem('class', JSON.stringify(list));
         }
-        else
-        {
-                list =  JSON.parse(list);
+        else {
+            list = JSON.parse(list);
         }
 
         var newId;
-        if(uuid==="")
-        {
+        if (uuid === "") {
             const date = new Date();
             newId = date.valueOf();
         }
-        else
-        {
+        else {
             newId = uuid;
         }
 
-        list.aaaa ={
-            a:"a",
-            b:"b",
-            c:"c",
+        list.aaaa = {
+            a: "a",
+            b: "b",
+            c: "c",
         }
 
         const object = {
-             COURSE_CODE: course_code,
-             COURSE_NAME: course_name,
-             LINK1:link1,
-             LINK2:link2,
-             LINK3:link3,
-             START_TIME:starttime,
-             END_TIME:endtime,
-             DAY:day,
+            SUBJECT_NAME: subjectName,
+            TEACHER_NAME: teacherName,
+            CLASS_CODE: classCode,
+            CLASS_GROUP: classGroup,
+            NO_OF_PERIOD: noOfPeriod,
+            COURSE_CODE: courseCode,
+            LINK1: link1,
+            START_TIME: startTime,
+            END_TIME: endTime,
+            DAY: day,
         }
 
         list[newId] = object;
-        await localStorage.setItem(type,JSON.stringify(list));
+        await localStorage.setItem('class', JSON.stringify(list));
     },
 
-    getLectures: async function(type,filter) {
+    getLectures: async function (type, filter) {
 
         const today = weekdays[getToday()];
-        var list = await localStorage.getItem(type);
-        if(list===null)
-        {
-                list={};
-                await localStorage.setItem(type,JSON.stringify(list));
+        var list = await localStorage.getItem('class');
+        if (list === null) {
+            list = {};
+            await localStorage.setItem('class', JSON.stringify(list));
 
         }
-        else
-        {
-                list =  JSON.parse(list);
+        else {
+            list = JSON.parse(list);
         }
-        var datalist =[];
+        var datalist = [];
         var editedItem;
         Object.entries(list).forEach(item => {
-            if(item[0]!=='aaaa') 
-            {
-                if(filter===true){
-                    if(item[1].DAY===today)
-                    {
+            if (item[0] !== 'aaaa') {
+                if (filter === true) {
+                    if (item[1].DAY === today) {
                         editedItem = item[1];
-                        editedItem["UUID"]=item[0];
+                        editedItem["UUID"] = item[0];
                         datalist.push(editedItem);
                     }
                 }
-                else{
-                        editedItem = item[1];
-                        editedItem["UUID"]=item[0];
-                        datalist.push(editedItem);
+                else {
+                    editedItem = item[1];
+                    editedItem["UUID"] = item[0];
+                    datalist.push(editedItem);
                 }
             }
         });
-        
+
         datalist.sort(comp);
         return datalist;
     },
 
-    clearTemp: function(){
-        localStorage.setItem("temp","");
+    clearTemp: function () {
+        localStorage.setItem("temp", "");
     },
 
-    deleteEntry: async function(type,uuid){
+    deleteEntry: async function (type, uuid) {
 
         var list = await JSON.parse(await localStorage.getItem(type));
-        
+
         delete list[uuid];
 
-        localStorage.setItem(type,JSON.stringify(list));
+        localStorage.setItem(type, JSON.stringify(list));
 
     }
 }
---
+++ src/pages/AddClass/AddClass.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { } from 'antd';
+import AddClassForm from '../../components/AddClassForm/AddClassForm';
+
+function AddClass() {
+    return (
+        <>
+            <AddClassForm />
+        </>
+    );
+};
+
+export default AddClass;
