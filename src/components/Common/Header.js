f30f39ed7bf5920113c66354a55bddd5be5c27e1
shreem011 <075BCT084.shreem@pcampus.edu.np>
2021-07-13 07:39:14 +0545

    ...

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -7,55 +7,43 @@ import "./common.css";
 const { Link } = Anchor;
 
 function AppHeader() {
-  const [visible, setVisible] = useState(false);
+	const [visible, setVisible] = useState(false);
 
-  const showDrawer = () => {
-    setVisible(true);
-  };
+	const showDrawer = () => {
+		setVisible(true);
+	};
 
-  const onClose = () => {
-    setVisible(false);
-  };
+	const onClose = () => {
+		setVisible(false);
+	};
 
-<<<<<<< HEAD
 	return (
 		<div className="container-fluid">
 			<div className="header">
-				{/* <div className="logo" /> */}
+				<div className="logo" />
 				<Router>
 					<Links to="/">
-						<h1 style={{ textAlign: "left" }}>BE Routine Management</h1>
+						<h1 style={{ textAlign: "left", color: "white" }}>
+							BE Routine Management
+						</h1>
 					</Links>
 				</Router>
 				{/* <a href="https://localhost:3000/">
-=======
-  return (
-    <div className="container-fluid">
-      <div className="header">
-        <div className="logo" />
-        <Router>
-          <Links to="/">
-            <h1 style={{ textAlign: "left", color: "white"}}>BE Routine Management</h1>
-          </Links>
-        </Router>
-        {/* <a href="https://localhost:3000/">
->>>>>>> 03e0e1c489607f4bd51deeed1b6276807ee4f42a
           
         </a> */}
 
-        {/* <h1 style={{textAlign: 'left'}}>BE Routine Management</h1> */}
-        <div className="mobileHidden">
-          <Anchor targetOffset="65">
-            <Link href="#hero" title="Home" />
-            <Link href="#about" title="About" />
-            <Link href="#feature" title="Features" />
-            <Link href="#faq" title="FAQ" />
-            <Link href="#contact" title="Contact" />
+				{/* <h1 style={{textAlign: 'left'}}>BE Routine Management</h1> */}
+				<div className="mobileHidden">
+					<Anchor targetOffset="65">
+						<Link href="#hero" title="Home" />
+						<Link href="#about" title="About" />
+						<Link href="#feature" title="Features" />
+						<Link href="#faq" title="FAQ" />
+						<Link href="#contact" title="Contact" />
 
-<<<<<<< HEAD
 						<div className="b">
 							<Router>
-								<Links to="/login">
+								<Links to="/user/login">
 									<Button className="btn" type="primary" size="large">
 										Login
 									</Button>
@@ -64,48 +52,34 @@ function AppHeader() {
 						</div>
 					</Anchor>
 				</div>
-=======
-            <div className="b">
-              <Router>
-                <Links to="/user/login">
-                  <Button className="btn" type="primary" size="large">
-                    Login
-                  </Button>
-                </Links>
-              </Router>
-            </div>
-          </Anchor>
-        </div>
->>>>>>> 03e0e1c489607f4bd51deeed1b6276807ee4f42a
 
-        <div className="mobileVisible">
-          <Button type="primary" onClick={showDrawer}>
-            <i className="fas fa-bars"></i>
-          </Button>
+				<div className="mobileVisible">
+					<Button type="primary" onClick={showDrawer}>
+						<i className="fas fa-bars"></i>
+					</Button>
 
-          <Drawer
-            placement="right"
-            closable={false}
-            onClose={onClose}
-            visible={visible}
-          >
-            <Anchor targetOffset="65">
-              <Link href="#hero" title="Home" />
-              <Link href="#about" title="About" />
-              <Link href="#feature" title="Features" />
-              <Link href="#faq" title="FAQ" />
-              <Link href="#contact" title="Contact" />
-              <div className="btnHolder">
-                <Button type="primary" size="large">
-                  Login
-                </Button>
-              </div>
-            </Anchor>
-          </Drawer>
-        </div>
-      </div>
-    </div>
-  );
+					<Drawer
+						placement="right"
+						closable={false}
+						onClose={onClose}
+						visible={visible}>
+						<Anchor targetOffset="65">
+							<Link href="#hero" title="Home" />
+							<Link href="#about" title="About" />
+							<Link href="#feature" title="Features" />
+							<Link href="#faq" title="FAQ" />
+							<Link href="#contact" title="Contact" />
+							<div className="btnHolder">
+								<Button type="primary" size="large">
+									Login
+								</Button>
+							</div>
+						</Anchor>
+					</Drawer>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AppHeader;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,77 +4,80 @@ import axios from "axios";
 import "./Login.css";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginStatus, setLoginStatus] = useState(false);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [loginStatus, setLoginStatus] = useState(false);
 
-  axios.defaults.withCredentials = true;
-  // axios.defaults.headers.common["x-auth-token"];
+	axios.defaults.withCredentials = true;
+	// axios.defaults.headers.common["x-auth-token"];
 
-  function handleUsernameInput(event) {
-    setUsername(event.target.value);
-  }
+	function handleUsernameInput(event) {
+		setUsername(event.target.value);
+	}
 
-  function handlePasswordInput(event) {
-    setPassword(event.target.value);
-  }
+	function handlePasswordInput(event) {
+		setPassword(event.target.value);
+	}
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const { data: user } = await axios({
-      method: "POST",
-      data: { username, password },
-      withCredentials: true,
-      url: "http://localhost:5000/user/login",
-    });
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const { data: user } = await axios({
+			method: "POST",
+			data: { username, password },
+			withCredentials: true,
+			url: "http://localhost:5000/user/login",
+		});
 
-    if (user) {
-      console.log(user.username);
-      localStorage.setItem("user", user.username);
-      setLoginStatus(true);
-      window.location = "/user/profile";
-    }
-  }
+		if (user) {
+			console.log(user.username);
+			localStorage.setItem("user", user.username);
+			setLoginStatus(true);
+			window.location = "/user/profile";
+		}
+	}
 
-  return (
-    <div>
-      <div id="wrap" className="wrapper">
-        <div className="title">Login</div>
-        <form onSubmit={handleSubmit}>
-          <div className="field">
-            <input
-              type="text"
-              required
-              onChange={handleUsernameInput}
-              autoFocus
-            />
-            <label>Email Address</label>
-          </div>
+	if (setLoginStatus == true) {
+	}
 
-          <div className="field">
-            <input type="password" required onChange={handlePasswordInput} />
-            <label>Password</label>
-          </div>
+	return (
+		<div>
+			<div id="wrap" className="wrapper">
+				<div className="title">Login</div>
+				<form onSubmit={handleSubmit}>
+					<div className="field">
+						<input
+							type="text"
+							required
+							onChange={handleUsernameInput}
+							autoFocus
+						/>
+						<label>User Name</label>
+					</div>
 
-          <div className="content">
-            <div className="checkbox">
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
+					<div className="field">
+						<input type="password" required onChange={handlePasswordInput} />
+						<label>Password</label>
+					</div>
 
-            <div className="pass-link">
-              <a href="#">Forgot password?</a>
-            </div>
-          </div>
+					<div className="content">
+						<div className="checkbox">
+							<input type="checkbox" id="remember-me" />
+							<label htmlFor="remember-me">Remember me</label>
+						</div>
 
-          <div className="field">
-            <input type="submit" value="Login" />
-          </div>
-          <div className="signup-link">
-            Not a member? <a href="#">Signup now</a>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+						<div className="pass-link">
+							<a href="#">Forgot password?</a>
+						</div>
+					</div>
+
+					<div className="field">
+						<input type="submit" value="Login" />
+					</div>
+					<div className="signup-link">
+						Not a member? <a href="#">Signup now</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
