73f5ed92851e45a9446718ae864f28257464680e
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2021-08-19 13:30:40 +0545

    pdf Generated

--- src/pages/Routine/Routine.js
+++ src/pages/Routine/Routine.js
@@ -64,27 +64,6 @@ const generatePDF = () => {
       pdf.save("Myroutine.pdf");
     },
   });
-
-  // var wb = XLSX.utils.table_to_book(document.getElementById("mytable"), {
-  //   sheet: "Sheet JS",
-  // });
-  // var wbout = XLSX.write(wb, {
-  //   bookType: "xlsx",
-  //   bookSST: true,
-  //   type: "binary",
-  // });
-  // function s2ab(s) {
-  //   var buf = new ArrayBuffer(s.length);
-  //   var view = new Uint8Array(buf);
-  //   for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
-  //   return buf;
-  // }
-  // $("#button-a").click(function () {
-  //   saveAs(
-  //     new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
-  //     "test.xlsx"
-  //   );
-  // });
 };
 
 for (let i = 0; i < teachersList.length; i++) {
@@ -205,6 +184,11 @@ export default function SpanningTable() {
             mode="single"
             allowClear
             style={{ width: "100%" }}
+            dropdownStyle={{
+              textAlign: "center",
+              backgroundColor: "#F2F1EF",
+              fontFamily: "cursive",
+            }}
             onChange={handleSelectedTeacher}
             placeholder="select teacher here"
           >
